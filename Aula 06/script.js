db42b50b68e4683b1bb7f1a46983f03d78433d4c
João Pedro <pedro.as2000@gmail.com>
2021-12-07 17:53:53 -0300

    Progresso dia: 07/12/2021

--- Aula 06/script.js
+++ Aula 06/script.js
@@ -1,9 +1,39 @@
-function calcular(){
-var mensagem = window.document.getElementById('resultado')
-var data = new Date()
-var dia = data.getDate()
-var mes = data.getMonth() + 1
-var ano = data.getFullYear()
-var idade = (31 - dian) + (365 - (mesn + 1) * 30) + (anoa - this.anon - 1) * 365 + diaa + (mesa * 30)
-mensagem.innerHTML
+try {
+    function calcular(){
+    var imagem = window.document.getElementById('imagem')
+    var mensagem = window.document.getElementById('resultado')
+    var agora = new Date()
+    var getNasc = window.document.getElementById('nasc')
+    var nasc = getNasc.value
+    var nascdate = new Date(nasc)
+    var dia = agora.getDate()
+    var mes = agora.getMonth() + 1
+    var ano = agora.getFullYear()
+    var dian = nascdate.getDate() + 1
+    var mesn = nascdate.getMonth() + 1
+    var anon = nascdate.getFullYear()
+    var idade = Number.parseInt(((31 - dian) + (365 - (mesn + 1) * 30) + (ano - anon - 1) * 365 + dia + (mes * 30)) / 365)
+    var button = window.document.getElementsByName('sex')
+    var sexo = ''
+    if(button[0].checked){
+        sexo = 'Masculino'
+    }
+    if(button[1].checked){
+        sexo = 'Feminino'
+    }
+    var peso = Number(window.document.getElementById('peso'))
+    var altura = Number(window.document.getElementById('altura'))
+    var imc = Number(peso / (altura * altura))
+    if(altura <= 0 || peso <= 0 || sexo == '' || idade < 0){
+        mensagem.innerHTML = `Preencha todos os dados e tente novamente`
+    }
+    else{
+        if(imc < 18.5){
+            imagem.src = 'abaixo_do_peso.png'
+        }
+    }
+}
+    
+} catch (error) {
+    mensagem.innerHTML = `Preencha todos os dados e tente novamente`
 }
