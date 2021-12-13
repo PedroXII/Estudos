2c3d7b55b16bede0c8f6baf3fff5b79c6b6e125b
João Pedro <pedro.as2000@gmail.com>
2021-12-13 17:11:26 -0300

    Progresso dia: 13/12/2021

--- Aula 07/script.js
+++ Aula 07/script.js
@@ -1,6 +1,8 @@
+//Não há motivo tecnico para ter as palavras 'get' e 'set' nas variáveis.
+
 try {
-    function calcular(){
-        var mensagem = window.document.getElementById('resultado')
+    function calcularcontador(){
+        var mensagem = window.document.getElementById('resultado-contador')
         var getInicio = window.document.getElementById('inicio')
         var setInicio = getInicio.value
         var inicio = Number(setInicio)
@@ -10,8 +12,70 @@ try {
         var getSequencia = window.document.getElementById('sequencia')
         var setSequencia = getSequencia.value
         var sequencia = Number(setSequencia)
-        for(inicio; inicio <= fim; inicio = inicio + sequencia){
-            mensagem.innerHTML = ``
+        var resultado = ''
+        var contador = 0
+        if(setFim.length == 0 || setInicio.length == 0 || setSequencia.length == 0 || sequencia == 0){
+            window.alert('Erro!');
+        }else{
+            if(inicio < fim){
+                for(inicio; inicio <= fim; inicio = inicio + sequencia){
+                    if(contador == 0){
+                        resultado = `inicio \u{1F449} ${inicio}` //\u{1F449} só funciona entre crases ``.
+                    }
+                    if(contador != 0){
+                        resultado = resultado + ' \u{1F449} ' + inicio
+                    }
+                    contador++
+                    mensagem.innerHTML = `${resultado}`
+                }
+                if(inicio >= fim){
+                    mensagem.innerHTML = `${resultado} \u{1F449} fim`
+                }
+            }
+            if(inicio > fim){
+                for(inicio; inicio >= fim; inicio = inicio - sequencia){
+                    if(contador == 0){
+                        resultado = `inicio \u{1F449} ${inicio}` //\u{1F449} só funciona entre crases ``.
+                    }
+                    if(contador != 0){
+                        resultado = resultado + ' \u{1F449} ' + inicio
+                    }
+                    contador++
+                    mensagem.innerHTML = `${resultado}`
+                }
+                if(inicio <= fim){
+                    mensagem.innerHTML = `${resultado} \u{1F449} fim`
+                }
+            }
+        }
+    }
+} catch (error) {
+    window.alert('Erro!')
+}
+// var e let é a mesma coisa.
+try {
+    function calculartabuada(){
+        let mensagem = window.document.getElementById('resultado-tabuada')
+        let inicio = 1
+        let getNumero = window.document.getElementById('numero')
+        let setNumero = getNumero.value
+        let numero = Number(setNumero)
+        let getFinalTabuada = window.document.getElementById('final-tabuada')
+        let setFinalTabuada = getFinalTabuada.value
+        let finaltabuada = Number(setFinalTabuada)
+        let tabuada = ''
+        let resultado
+        if(setNumero.length == 0 || setFinalTabuada.length == 0){
+            window.alert('Erro!')
+        }
+        else{
+            do{
+                resultado = numero * inicio
+                tabuada = `${tabuada}${numero} * ${inicio} = ${resultado}<br/>`
+                mensagem.innerHTML = tabuada
+                inicio++
+            }
+            while(inicio <= finaltabuada);
         }
     }
 } catch (error) {
